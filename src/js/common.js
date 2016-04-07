fb308566b550bfd4df6b73bd471a6686ff52b96c
Watch1k <host.sf@gmail.com>
2016-04-08 00:02:11 +0300

    fix js error

--- src/js/common.js
+++ src/js/common.js
@@ -35,7 +35,7 @@ head.ready(function(){
 	// Clear placeholder
 	$(function() {
 		$('input,textarea').focus(function(){
-				$(this).data('placeholder',$(this).attr('placeholder'))
+				$(this).data('placeholder',$(this).attr('placeholder'));
 				$(this).attr('placeholder','');
 		});
 		$('input,textarea').blur(function(){
